34881e56abc19e9d52cf5bbc69f5a3018ba673c7
Racheldhia <dhiarachella@gmail.com>
2026-05-29 20:29:37 +0700

    login,register,spashscreen page

--- app/src/main/java/id/hanifalfaqih/todoapp/ui/SplashFragment.kt
+++ app/src/main/java/id/hanifalfaqih/todoapp/ui/SplashFragment.kt
@@ -1,28 +1,26 @@
 package id.hanifalfaqih.todoapp.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import id.hanifalfaqih.todoapp.R
 
-class EditProfileFragment : Fragment() {
+class SplashFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_edit_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_splash, container, false)
         
-        view.findViewById<ImageButton>(R.id.btn_back).setOnClickListener {
-            findNavController().popBackStack()
-        }
-        
-        view.findViewById<View>(R.id.btn_save_profile).setOnClickListener {
-            findNavController().popBackStack()
-        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splash_to_login)
+        }, 2000)
         
         return view
     }
