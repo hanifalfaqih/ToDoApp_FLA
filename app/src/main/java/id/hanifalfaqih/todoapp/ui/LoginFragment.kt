d9b3617454ae8efcb2a40df49441d2a50b076196
Maulana Hanif Al Faqih Rojichan <maulana.rojichan@binus.ac.id>
2026-06-06 11:09:30 +0700

    Use AppModule to provide ViewModelFactory and update login hint

--- app/src/main/java/id/hanifalfaqih/todoapp/ui/LoginFragment.kt
+++ app/src/main/java/id/hanifalfaqih/todoapp/ui/LoginFragment.kt
@@ -14,6 +14,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import id.hanifalfaqih.todoapp.R
+import id.hanifalfaqih.todoapp.di.AppModule
 import id.hanifalfaqih.todoapp.di.ViewModelFactory
 import id.hanifalfaqih.todoapp.presentation.auth.login.LoginViewModel
 import id.hanifalfaqih.todoapp.presentation.common.Event
@@ -22,7 +23,7 @@ import kotlinx.coroutines.launch
 class LoginFragment : Fragment() {
 
     private val viewModel: LoginViewModel by viewModels {
-        ViewModelFactory(requireContext())
+        AppModule.provideViewModelFactory(requireContext())
     }
 
     override fun onCreateView(
